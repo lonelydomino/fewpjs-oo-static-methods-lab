944f1d5af09e55fffdd9d902d3510d2758900a7c
Michael Martinez <zeroeclipse@live.com>
2021-06-13 15:17:17 -0700

    commit tree 1a33fb3899d84157834ae143f3109fee1b0dd112

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+  static titleize( words ) {
+    let items = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let finalArray = [];
+    let startingArray = words.split(" ")
+    for (let i = 0;i < startingArray.length; i++) {
+      if (i == 0) {
+        finalArray.push(this.capitalize(startingArray[i]))
+      } else {
+        if (items.includes(startingArray[i])){
+          finalArray.push(startingArray[i])
+        } else {
+          finalArray.push(this.capitalize( startingArray[i]))
+        }
+      }
+
+    }
+    return finalArray.join(" ");
+  }
 }
